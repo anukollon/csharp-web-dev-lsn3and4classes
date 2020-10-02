071e252103eed2210380b8ca28f1cac0c3aa4a92
anukollon <anukollon@gmail.com>
2020-10-02 17:01:51 -0500

    Chapter3 Exercise

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,9 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student student = new Student();
+            student.NumberOfCredits = 1;
+            student.Gpa = 4.0;
         }
     }
 }
---
+++ SchoolPractice/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Teacher
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Subject { set; get; }
+        public string YearsTeaching { set; get; }
+
+    }
+}
